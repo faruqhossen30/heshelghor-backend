11b028cef1a04ade17c32b8585d5a34d6d544cf3
Faruq Hossen <faruqhossen30@gmail.com>
2021-09-07 18:47:38 +0600

    product table migration file create

--- database/migrations/2021_08_29_064927_create_products_table.php
+++ database/migrations/2021_08_29_064927_create_products_table.php
@@ -15,6 +15,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('slug');
+            $table->string('catagory_id');
+            $table->string('brand_id');
+            $table->integer('user_id');
+            $table->integer('Vendor_id');
+            $table->integer('price');
+            $table->integer('regular_price')->nullable();
+            $table->integer('sale_price')->nullable();
+            $table->integer('quantity');
+            $table->integer('puk_code');
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
